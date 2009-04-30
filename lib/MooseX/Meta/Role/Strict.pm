55ed82e58c8a7f832b6ce666aca55d319f5f6e40
Shawn M Moore <sartak@gmail.com>
2009-05-01 00:40:59 +0800

    Future-proof role's apply
    Signed-off-by: Ovid <ovid@cpan.org>

--- lib/MooseX/Meta/Role/Strict.pm
+++ lib/MooseX/Meta/Role/Strict.pm
@@ -5,28 +5,17 @@ extends 'Moose::Meta::Role';
 
 our $VERSION = 0.01;
 
-sub apply {
+override apply => sub {
     my ( $self, $other, @args ) = @_;
 
-    ( blessed($other) )
-      || Moose->throw_error("You must pass in an blessed instance");
-
-    if ( $other->isa('Moose::Meta::Role') ) {
-        require Moose::Meta::Role::Application::ToRole;
-        return Moose::Meta::Role::Application::ToRole->new(@args)
-          ->apply( $self, $other );
-    }
-    elsif ( $other->isa('Moose::Meta::Class') ) {
+    if ( blessed($other) && $other->isa('Moose::Meta::Class') ) {
         # already loaded
         return MooseX::Meta::Role::Application::ToClass::Strict->new(@args)
           ->apply( $self, $other );
     }
-    else {
-        require Moose::Meta::Role::Application::ToInstance;
-        return Moose::Meta::Role::Application::ToInstance->new(@args)
-          ->apply( $self, $other );
-    }
-}
+
+    super;
+};
 
 1;
 
